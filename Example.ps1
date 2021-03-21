5c316ff6887fff82a4c14b6d600e7359011b0c64
Thaison Nguyen <thieson08@me.com>
2021-03-21 08:22:01 +0100

    Fix Loading status

--- Example.ps1
+++ Example.ps1
@@ -26,10 +26,10 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  #>
 
- Import-Module -Name ./PSWriteStatus.psm1
+ Import-Module -Name ./PSWriteStatus.psm1 -Force
 
  while ($true) {
-    "Test" | Write-Status -Loading $true
+    "Test" | Write-Status -Loading
     Start-Sleep -Milliseconds 100
 
  }
